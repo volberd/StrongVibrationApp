4ff2ea67292ea2a8844b08b9e612f63106e05234
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2024-01-30 14:14:48 +0200

    first commit

---
+++ StrongBibration/StrongBibration/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  StrongBibration
+//
+//  Created by newAccount on 30.01.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ StrongBibration/StrongBibration/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  StrongBibration
+//
+//  Created by newAccount on 30.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+}
+
